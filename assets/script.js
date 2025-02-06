3f4c1c066dbfd5d5b903f0d4d7364f934e37959e
aashikur <mdaashikur@gmail.com>
2025-02-06 14:04:33 +0600

    hello

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,9 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
+
+    window.addEventListener('click', function() {
+      var audio = document.getElementById('background-audio');
+      audio.muted = false;  // Unmute when user clicks anywhere
+      audio.play();
+    }, { once: true });  // Ensures it runs only once
